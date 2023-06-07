9a76a34ac56f9a807af061af82fba3b9be6739ab
ostap <127669847+ostapzat@users.noreply.github.com>
2023-06-07 10:41:51 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,3 +10,19 @@ function toggleAccordion(element) {
   accordionContent.classList.toggle("active");
 }
 
+// Отримуємо основний елемент сторінки
+// Отримуємо основний елемент сторінки
+var bodyElement = document.body;
+
+// Встановлюємо overflow-x: hidden на елемент body
+bodyElement.style.overflowX = "hidden";
+
+// Функція, яка перевіряє розмір вікна та оновлює ширину сторінки
+function updatePageWidth() {
+  var windowWidth = window.innerWidth;
+  mainElement.style.width = windowWidth + "px";
+}
+
+// Оновлюємо ширину сторінки при завантаженні та зміні розміру вікна
+window.addEventListener("load", updatePageWidth);
+window.addEventListener("resize", updatePageWidth);
