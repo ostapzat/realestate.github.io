925d53ba7fffc9d201812ca9dd305b65d7f45b98
ostap <127669847+ostapzat@users.noreply.github.com>
2023-06-07 15:14:21 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,3 +26,70 @@ function updatePageWidth() {
 // Оновлюємо ширину сторінки при завантаженні та зміні розміру вікна
 window.addEventListener("load", updatePageWidth);
 window.addEventListener("resize", updatePageWidth);
+
+jQuery(document).ready(function ($) {
+  $(".regular").slick({
+    dots: true,
+    //       infinite: true,
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    // autoplay: true,
+    //       arrows: true,
+    //       mobileFirst: true,
+    //       easing: 'easeOutElastic',
+    //       speed: 800,
+    autoplay: true,
+    autoplaySpeed: 9000,
+    speed: 700,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    respondTo: "min",
+    cssEase: "linear",
+    prevArrow: '<span class="icon-angle-left"></span>',
+    nextArrow: '<span class="icon-angle-right"></span>',
+  });
+
+  $(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    draggable: false,
+    fade: true,
+    asNavFor: ".slider-nav",
+  });
+  $(".slider-nav").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: false,
+    arrows: true,
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: "10px",
+    prevArrow: '<span class="icon-angle-left"></span>',
+    nextArrow: '<span class="icon-angle-right"></span>',
+    responsive: [
+      {
+        breakpoint: 450,
+        settings: {
+          dots: false,
+          slidesToShow: 3,
+          centerPadding: "0px",
+        },
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          autoplay: true,
+          dots: false,
+          slidesToShow: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  });
+});
+
+
